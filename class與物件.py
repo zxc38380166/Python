be6f38a8b143550b2a8d23bad520d667f39f4dee
Barry <zxc38380166@yahoo.com.tw>
2021-12-31 16:09:22 +0800

    12/31

--- class與物件.py
+++ class與物件.py
@@ -4,8 +4,20 @@ class Phone:
         self.number = number
         self.is_waterproof = is_waterproof
 
+    def is_ios(self):                  #在類別中也可以定義函式供呼叫
+        if self.os == "ios":            #若自身的os為"ios"則回傳True
+            return True
+        else:
+            return False
+
+    def add(self,number1,number2):      #類別中的函式也可以拿來做運算
+        return number1+number2
+
 phone1 = Phone("ios",123,True)  #帶入屬性的值
 phone2 = Phone("andriod",456,False)
-print(phone1.os)
-print(phone2.number)
-        
+
+print( phone1.os)
+print( phone2.number)
+print( phone1.is_ios() )                #呼叫類別中的函式
+print( phone2.is_ios() )
+print( phone2.add(5,6) )                #呼叫類別中的函式用來運算
